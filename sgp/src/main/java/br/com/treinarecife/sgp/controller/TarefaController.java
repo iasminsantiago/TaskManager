8adf6cad24dec2acb55b9d61a5580e6144821fe7
Iasmin Codes <ia.smincodes@gmail.com>
2026-01-31 13:43:41 -0300

    Update TarefaController.java

--- sgp/src/main/java/br/com/treinarecife/sgp/controller/TarefaController.java
+++ sgp/src/main/java/br/com/treinarecife/sgp/controller/TarefaController.java
@@ -2,10 +2,10 @@ package br.com.treinarecife.sgp.controller;
 
 import java.util.List;
 
-/* // linha 1 - package
+// linha 1 - package
 // linha 2 - cores do java java....
 // linha 3 imports acssorios como springframework
-// linha 4 - import dos codigos que eu criei  */
+// linha 4 - import dos codigos que eu criei  
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.treinarecife.sgp.model.Tarefa;
 import br.com.treinarecife.sgp.repository.TarefaRepository;
 
-/* // rest controller recebe as requisições de classe
-// request mapping é o caminho da rota 
-// NUNCA SEPARAR @ da classe que vem logo abaixo, pois diz-se que @restcontroller alerarrá pela linha de baixo, ai ao inves de vir a classe vem um comentaroo, ele nao entende */
-
-
-// 4 verbos basicos: post get pra todos  get especifico  pt para atualziar um id especifico e rota delete pra apagar um id especifico
 
 @RestController
 @RequestMapping("/api/tarefas")
@@ -34,25 +28,19 @@ public class TarefaController {
     @Autowired  
     private TarefaRepository tarefaRepository;
     
-    // rota POST:
-    // deve ter obrigatoriamente um copo de dado, ai uso requestbody para pegar ele
+
     @PostMapping 
     public Tarefa postTarefa(@RequestBody Tarefa tarefa){
         return tarefaRepository.save(tarefa);
-        // ele vai la no repositorio e salva os dados que vieram pela variavel tarefa
+        // Ele vai la no repositorio e salva os dados que vieram pela variavel tarefa
     }
 
     // Rota PUT para atualizar um ID especifico
-    // pit mapping é uma rota, entao ela tem que recebr um apramentro especifico
-    // o id nao é atualziado, nao faz sentido
-    // lombok ja criou os metodos get e set pra mim, nao preciso esrever os metosos ge te set, so s usar
-    // CORREÇÀO!!!!!!!!!!  return tarefaRepository.findById().ma... -> return tarefaRepository.findById(id).ma...
+
     @PutMapping("/{id}")
     public ResponseEntity<Tarefa> putTarefa(@PathVariable Long id, @RequestBody Tarefa tarefaDetails) {
         return tarefaRepository.findById(id).map(tarefa -> {
             tarefa.setTitulo(tarefaDetails.getTitulo());
-            // !!!!!!!!repetir para os outros campos, como descricao, titulo etc!!!!!!!
-            // CORREÇÃO!!!!!!!!!! INSERIMOS OS OUTROS CAMPOS. 
             // agora o PUT atualiza tudo
             tarefa.setDescricao(tarefaDetails.getDescricao());
             tarefa.setStatus(tarefaDetails.getStatus());
@@ -63,10 +51,9 @@ public class TarefaController {
             return ResponseEntity.ok(updateTarefa);
         }).orElse(ResponseEntity.notFound().build());
         
-        // preciso encontrar o id par aper o atualizar, chamao
     }
 
-    // retornará uma lista de tarefas
+    // Retornará uma lista de tarefas
     // get all:
     @GetMapping
     public List<Tarefa> getAllTarefas() {
@@ -74,7 +61,6 @@ public class TarefaController {
     }
 
     // get id:
-    // CORREÇÃO!!!!!!!!!!!      @GetMapping("/(id)") ->     @GetMapping("/{id}")
     @GetMapping("/{id}")
     public ResponseEntity<Tarefa> getTarefaById(@PathVariable Long id) {
         return tarefaRepository.findById(id).map(ResponseEntity::ok)
@@ -82,20 +68,8 @@ public class TarefaController {
 
     }
 
-    // end point é ponto final, rota.  a base url rodnao na minha maquina é lcoalhost:8080, essa é a base url. abaxo dela, colocamos os endpoint: 
-    ///api/usuario.
-    /// meu usuaruo tem 3 metoos rest: post, get, put, delete
-    /// /api/usuarios
-    ///     post(id)  tme qeu ser nitario
-    ///     get(all, id) 
-    ///     put(id)  obriatorio ele se runitario, tem qru ter id
-    ///     delete(id)    essas coisa snoa entram na rota, 
-    // a rota é api/usuario
-   // a abse url é o  localost:8080
-  // pra fazer algo num id espeiifco, faço /api/usuario/{id}
-
-
 
 
 }
+
 
