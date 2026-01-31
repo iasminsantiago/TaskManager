b738e5b049cbbcdc037edf3951e22c863d17ca5c
Iasmin Codes <ia.smincodes@gmail.com>
2026-01-31 13:25:36 -0300

    Update TarefaController.java

--- sgp/src/main/java/br/com/treinarecife/sgp/controller/TarefaController.java
+++ sgp/src/main/java/br/com/treinarecife/sgp/controller/TarefaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,7 +61,7 @@ public class TarefaController {
             tarefa.setUsuario(tarefaDetails.getUsuario());
             Tarefa updateTarefa = tarefaRepository.save(tarefa);
             return ResponseEntity.ok(updateTarefa);
-        }).orElse(ResponseEntity.notfound().build());
+        }).orElse(ResponseEntity.notFound().build());
         
         // preciso encontrar o id par aper o atualizar, chamao
     }
@@ -97,3 +98,4 @@ public class TarefaController {
 
 
 }
+
