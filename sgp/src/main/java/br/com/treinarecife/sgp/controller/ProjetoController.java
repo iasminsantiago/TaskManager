f2ecef20792cca55a87f08e7d60c54a8f34fc459
Iasmin Codes <ia.smincodes@gmail.com>
2026-01-31 13:42:14 -0300

    Update ProjetoController.java

--- sgp/src/main/java/br/com/treinarecife/sgp/controller/ProjetoController.java
+++ sgp/src/main/java/br/com/treinarecife/sgp/controller/ProjetoController.java
@@ -16,34 +16,37 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.treinarecife.sgp.model.Projeto;
 import br.com.treinarecife.sgp.repository.ProjetoRepository;
 
-/* // rest controller recebe as requisições de classe
-// request mapping é o caminho da rota 
-// NUNCA SEPARAR @ da classe que vem logo abaixo, pois diz-se que @restcontroller alerarrá pela linha de baixo, ai ao inves de vir a classe vem um comentaroo, ele nao entende */
+/* Rest controller recebe as requisições de classe. 
+// Request mapping é o caminho da rota.
+// Obs.: Não separamos @ da classe que vem logo abaixo. Ele se orientará pela linha de baixo. Se vir um comentário antes dela, ele não entenderá o comando.
 
 
-// 4 verbos basicos: post get pra todos  get especifico  pt para atualziar um id especifico e rota delete pra apagar um id especifico
+/* 4 verbos basicos: 
+post get pra todos  
+get especifico  
+put para atualziar um id especifico e rota 
+delete pra apagar um id especifico */
 
 @RestController
 @RequestMapping("/api/projetos")
 public class ProjetoController {
-    // ligar controller com meu repositorio usando a annotation autowired:
+    // Ligar controller com meu repositorio usando a annotation autowired:
     @Autowired  
     private ProjetoRepository projetoRepository;
     
-    // rota POST:
-    // deve ter obrigatoriamente um copo de dado, ai uso requestbody para pegar ele
+    // Rota POST:
+    // deve ter obrigatoriamente um corpo de dado, e uso requestbody para pegar ele
     @PostMapping 
     public Projeto postProjeto(@RequestBody Projeto projeto){
         return projetoRepository.save(projeto);
-        // ele vai la no repositorio e salva os dados que vieram pela variavel projeto
+        // Ele vai la no repositório e salva os dados que vieram pela variavel projeto.
     }
 
-    // Rota PUT para atualizar um ID especifico
-    // pit mapping é uma rota, entao ela tem que recebr um apramentro especifico
-    // o id nao é atualziado, nao faz sentido
-    // lombok ja criou os metodos get e set pra mim, nao preciso esrever os metosos ge te set, so s usar
-    // CORREÇÃO!!!!!!!!!!!!  projeto.setTItulo(projetoDetails.getTitulo());  ->    projeto.setNome(projetoDetails.getNome());
-    // CORREÇÀO!!!!!!!!!!!   return projetoRepository.findById().map(projeto -> {        return projetoRepository.findById(id).map(projeto -> {
+    /* Rota PUT para atualizar um ID especifico 
+    put mapping é uma rota, entao ela tem que receber um paramentro especifico Senão, o id nao é atualizado, nao faz sentido.
+    lombok ja criou os metodos get e set pra mim, nao preciso esrever os metosos get e set */ 
+    
+   
     @PutMapping("/{id}")
     public ResponseEntity<Projeto> putProjeto(@PathVariable Long id, @RequestBody Projeto projetoDetails) {
         return projetoRepository.findById(id).map(projeto -> {
@@ -53,17 +56,15 @@ public class ProjetoController {
             return ResponseEntity.ok(updateProjeto);
         }).orElse(ResponseEntity.notFound().build());
         
-        // preciso encontrar o id par aper o atualizar, chamao
     }
 
-    // retornará uma lista de projetos
     // get all:
+   // Retornará uma lista de projetos
     @GetMapping
     public List<Projeto> getAllProjetos() {
         return projetoRepository.findAll();
     }
 
-    // CORREÇÃO!!!!!!!!!!!!!!     @GetMapping("/(id)") ->    @GetMapping("/{id}")
     // get id:
     @GetMapping("/{id}")
     public ResponseEntity<Projeto> getProjetoById(@PathVariable Long id) {
@@ -72,20 +73,22 @@ public class ProjetoController {
 
     }
 
-    // end point é ponto final, rota.  a base url rodnao na minha maquina é lcoalhost:8080, essa é a base url. abaxo dela, colocamos os endpoint: 
-    ///api/usuario.
-    /// meu usuaruo tem 3 metoos rest: post, get, put, delete
-    /// /api/usuarios
-    ///     post(id)  tme qeu ser nitario
-    ///     get(all, id) 
-    ///     put(id)  obriatorio ele se runitario, tem qru ter id
-    ///     delete(id)    essas coisa snoa entram na rota, 
-    // a rota é api/usuario
-   // a abse url é o  localost:8080
-  // pra fazer algo num id espeiifco, faço /api/usuario/{id}
+   /* end point é ponto final, rota.  
+   A base url rodando  na minha maquina é localhost:8080, essa é a base url. Abaxo dela, colocamos os endpoints: 
+    /api/usuario.
+    Meu usuaruo tem 3 metodos rest: post, get, put, delete
+     /api/usuarios
+        post(id)  tme qeu ser nitario
+        get(all, id) 
+        put(id)  obriatorio ele se runitario, tem qru ter id
+        delete(id)    essas coisa snoa entram na rota, 
+   A rota é api/usuario
+   A base url é o  localost:8080
+  Parara fazer algo num id espeiifco, faço /api/usuario/{id} */
 
 
 
 
 }
+
 
